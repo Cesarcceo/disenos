a84d33ac3e3c2be76ad16af389191b9596270705
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-04-25 10:26:08 -0600

    New feature(card_table.dart): :sparkles: se agrego un efecto de difuminado al fondo debajo de las cartas

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -40,23 +42,31 @@ class _SingleCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 250,
       margin: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color:const Color.fromRGBO(62, 66, 107, 0.7),
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: color,
-            radius: 50,
-            child: Icon(icon, size: 55, color: Colors.white,),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+          child: Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color:const Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: color,
+                  radius: 50,
+                  child: Icon(icon, size: 55, color: Colors.white,),
+                ),
+                const SizedBox(height: 10,),
+                Text(text, style:  TextStyle(color: color, fontSize: 18),)
+              ],
+            ),
           ),
-          const SizedBox(height: 10,),
-          Text(text, style:  TextStyle(color: color, fontSize: 18),)
-        ],
+        ),
       ),
     );
   }
